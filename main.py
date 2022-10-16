045869a3bb2fd45dcbd86ace233bbcb78729abc5
rakesh1268248 <114190126+rakesh1268248@users.noreply.github.com>
2022-10-16 15:31:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,73 +1,85 @@
-import fitz # install using: pip install PyMuPDF
-import string
+import fitz
+import io
+import docx
+from pptx import Presentation
 import re
+import string
 import nltk
+import streamlit as st
 nltk.download('all')
-from nltk import sent_tokenize,word_tokenize
-from nltk.corpus import stopwords  #stopwords
-from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+stop_words=set(nltk.corpus.stopwords.words('english'))
 lemmatizer = WordNetLemmatizer()
-stemmer=PorterStemmer()
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
+from rank_bm25 import *
 
-
+#for adding daisi dependencies
+# import pydaisi as pyd
+# stable_diffusion = pyd.Daisi("laiglejm/Stable Diffusion")
 
 def return_doc_from_bytes(pdfbytes):
   doc = fitz.open(stream=pdfbytes)
   return doc
 
-def preprocessing(sentences):
-  documents_clean = ''
-  for d in sentences:
-      # Remove Unicode
-      document_test = re.sub('[^a-zA-Z0-9]', ' ', d)
-      # Lowercase the document
-      document_test = document_test.lower()
-      # Remove punctuations
-      document_test = re.sub(r'[%s]' % re.escape(string.punctuation), ' ', document_test)
-      #Remove the numbers
-      document_test = re.sub(r'[0-9]', '', document_test)
-      # Remove the doubled space
-      document_test = re.sub(r'\s{2,}', ' ', document_test)
-      #tokenization
-      document_test = document_test.split()
-      #stopwords_removal
-      document_test = [word for word in document_test if not word in set(stopwords.words('english'))]
-      #stemming
-      #document_test = [stemmer.stem(word) for word in document_test]
-      #lemmmitization
-      document_test = [lemmatizer.lemmatize(word) for word in document_test]
-      document_test = ' '.join(document_test)
-      documents_clean+=(document_test)
-  # print(documents_clean)
+def preprocessing(documents):
+  documents_clean = []
+  for d in documents:
+    # Remove Unicode
+    document_test = re.sub('[^a-zA-Z0-9]', ' ', str(d))
+    # Remove Mentions
+    document_test = re.sub(r'@\w+', '', document_test)
+    # Lowercase the document
+    document_test = document_test.lower()
+    # Remove punctuations
+    document_test = re.sub(r'[%s]' % re.escape(string.punctuation), ' ', document_test)
+    # Lowercase the numbers
+    document_test = re.sub(r'[0-9]', '', document_test)
+    # Remove the doubled space
+    document_test = re.sub(r'\s{2,}', ' ', document_test)
+    #tokenization
+    document_test = document_test.split()
+    #lemmmitization
+    document_test = [lemmatizer.lemmatize(word) for word in document_test if not word in set(stopwords.words('english'))]
+    document_test = ' '.join(document_test)
+    documents_clean.append(document_test)
+  return documents_clean
 
-
-def get_wordcloud():
+def search_report(documents_clean,query):
+  tokenized_corpus = [doc.split(" ") for doc in documents_clean]
+  bm25 = BM25Okapi(tokenized_corpus)
+  tokenized_query = query.split()
+  # doc_scores = bm25.get_scores(tokenized_query)
+  result=bm25.get_top_n(tokenized_query,documents_clean , n=5)
+  return result
+def st_ui():
   st.set_page_config(layout = "wide")
-  st.title("Word Cloud of supply contract")
+  st.title("Auto Review Legal contracts - DocumentAI")
+    
   fileupload = st.sidebar.file_uploader("Upload a Contract here")
-  Button=st.sidebar.button('Get wordcloud')
+  select_category = st.sidebar.selectbox("select_category", ["category", "PDF", 'Word Document','PPT'])
+  Enter_text = st.sidebar.text_input("Text to search", "please enter the text")
+  st.text(fileupload)
+  Button=st.sidebar.button('Analyze_contract')
    
   if fileupload is not None:
-    text=''
-    
-    pdfbytes = fileupload.getvalue()
-    doc = return_doc_from_bytes(pdfbytes)
-    for page in doc:
-      text+=(page.get_text()
-    sentences = nltk.sent_tokenize(text)
-    cleaned_document=preprocessing(sentences)
-    wordcloud = WordCloud(width = 800, height =600,background_color ='white',min_font_size = 5,max_words=500).generate(cleaned_document)
-    # plot the WordCloud image                      
-    plt.figure(figsize = (15,10), facecolor = None)
-    plt.imshow(wordcloud,interpolation="bilinear")
-    plt.axis("off")
-    plt.tight_layout(pad = 0)
-
-    plt.show()
+    text=[]
+    if select_category == "PDF":
+      pdfbytes = fileupload.getvalue()
+      doc = return_doc_from_bytes(pdfbytes)
+      for page in doc:
+        text+=(page.get_text().split('\n'))
+    elif select_category =="Word Document":
+      doc = docx.Document(fileupload)
+      for i in range(len(doc.paragraphs)):
+        text.append(doc.paragraphs[i].text)
+  st.text('debug point 1')
+  st.text(text)
+  cleaned_document=preprocessing(text)
+  st.text('debug point 2')
+  if Enter_text:
+    result=search_report(cleaned_document,Enter_text)
+  st.text(result)
  
 
 if __name__ == "__main__":
-    get_wordcloud()
+    st_ui()
