8e5784fbe86058bfc8a812324256b5677e6d3600
rakesh1268248 <114190126+rakesh1268248@users.noreply.github.com>
2022-10-23 18:44:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -61,7 +61,9 @@ def st_ui():
   st.set_page_config(layout = "wide")
   st.title("Auto Review Legal contracts - DocumentAI")  
   fileupload = st.sidebar.file_uploader("Upload a Contract here")
-  select_category = st.sidebar.selectbox("select_category", ["category", "Content Analytics", "Risk Analytics","Search"])
+  select_category = st.sidebar.selectbox("select_category", ["Summarization", "Sentiment Analytics", "Risk Analytics","Price Analytics","People/Stakeholders Analytics","Spatial Analytics",
+                                                             "Text To Search","Grid Analytics","Social Analytics","Conversation-Transcripts Analytics","Non English Text","Filter Non English",
+                                                            "List Of Languages","Display Full English Version","Full Document Translation"])
   Button=st.sidebar.button('content Analytics')
   #button=st.sidebar.button('Risk Analytics')
   Enter_text = st.sidebar.text_input("Text to search")
